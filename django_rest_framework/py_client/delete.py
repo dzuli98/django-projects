fce0accc9335c395deee10b2987d327aadfca58b
dzuli98 <filipovicjulija98@gmail.com>
2025-01-13 17:08:40 +0100

    Delete django_rest_framework directory

--- django_rest_framework/py_client/delete.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-product_id = 9
-
-endpoint = f"http://localhost:8000/api/products/{product_id}/delete/"
-get_response = requests.delete(endpoint)
-print(get_response.status_code)
-
